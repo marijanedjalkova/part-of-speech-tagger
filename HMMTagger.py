e513564bc6ec111fb175534a2831ffcbb07df227
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-02-07 17:07:28 +0000

    added next_tag and fixed naming issues with tagger

--- HMMTagger.py
+++ HMMTagger.py
@@ -2,6 +2,7 @@ class HMMTagger(object):
 
     def __init__(self, n=2):
         self.n = n
+        self.freqDist = CFFreqDist()
 
     def tag(self, testing_tokens):
         return []
@@ -10,4 +11,19 @@ class HMMTagger(object):
         history = []
 
         for token in training_tokens:
-            context = tuple(history,  + [])
+            context = tuple(history + [token[0]])
+            feature = token[1]
+            self.freqDist[context].inc(feature)
+            history.append(token[1])
+            if len(history) == (self.n+1):
+                del history[0]
+
+    def next_tag(self, tagged_tokens, next_token):
+        # Find the tags of the n previous tokens.
+        history = []
+        start = max(len(tagged_tokens) - self.n, 0)
+        for token in tagged_tokens[start:]:
+            history.append(token[1])
+            # Return the most likely tag for the token’s context.
+            context = tuple(history + [next_token.type()])
+            return self.freqDist[context].max()
