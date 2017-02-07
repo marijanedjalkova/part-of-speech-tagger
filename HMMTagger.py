8212fb7b6686e130e943350e79ac970cd08b6654
marijanedjalkova <nedyalkova.marya@gmail.com>
2017-02-07 23:00:45 +0000

    training done

--- HMMTagger.py
+++ HMMTagger.py
@@ -1,22 +1,24 @@
+from nltk import ConditionalFreqDist
+
 class HMMTagger(object):
 
     def __init__(self, n=2):
         self.n = n
-        self.freqDist = CFFreqDist()
+        self.freqDist = ConditionalFreqDist()
 
     def tag(self, testing_tokens):
         return []
 
-    def train(self, training_tokens):
+    def train(self, training_sents):
         history = []
-
-        for token in training_tokens:
-            context = tuple(history + [token[0]])
-            feature = token[1]
-            self.freqDist[context].inc(feature)
-            history.append(token[1])
-            if len(history) == (self.n+1):
-                del history[0]
+        for sent in training_sents:
+            for token in sent:
+                context = tuple(history + [token[0]])
+                feature = token[1]
+                self.freqDist[context][feature] += 1
+                history.append(token[1])
+                if len(history) == (self.n+1):
+                    del history[0]
 
     def next_tag(self, tagged_tokens, next_token):
         # Find the tags of the n previous tokens.
@@ -24,6 +26,8 @@ class HMMTagger(object):
         start = max(len(tagged_tokens) - self.n, 0)
         for token in tagged_tokens[start:]:
             history.append(token[1])
-            # Return the most likely tag for the token’s context.
+
             context = tuple(history + [next_token.type()])
+            print "context:"
+            print context
             return self.freqDist[context].max()
--- main.py
+++ main.py
@@ -9,11 +9,14 @@ def main():
     # 57340 sentences
     training_set = sents[:50000]
     testing_set = sents[50000:50500]
+    t = HMMTagger()
+    t.train(training_set)
+    print t.next_tag(["AP"], "sick")
+
+
 
 def compare(detected_tags_lst, original_tags_lst):
     return (sum([1 if x == y else 0 for x, y in zip(detected_tags, original_tags)]), len(original_tags))
 
 if __name__ == '__main__':
-    #main()
-    t = HMMTagger()
-    t.tag([])
+    main()
