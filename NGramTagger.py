49bd59284341d9d2dd963d08b27dc9017ae33f30
Marija Nedjalkova nedyalkova.marya@gmail.com <nedyalkova.marya@gmail.com>
2017-02-13 13:24:43 +0000

    code cleanup and commenting

--- NGramTagger.py
+++ NGramTagger.py
@@ -1,22 +1,17 @@
 from nltk import ConditionalFreqDist
 
-class HMMTagger(object):
+class NGRamTagger(object):
+    """ N-Gram Part-of-Speech tagger """
 
     def __init__(self, n=0):
+        """ Constructor.
+        n - the number of previous tags considered
+        freqDist - a condiditional frequency distribution """
         self.n = n
         self.freqDist = ConditionalFreqDist()
 
-    def tag(self, tokenized_sents):
-        res = []
-        for sentence in tokenized_sents:
-            tagged_sentence = []
-            for token in sentence:
-                context = tagged_sentence[-self.n:]
-                tagged_sentence.append(self.next_tag(context, token))
-            res.append(tagged_sentence)
-        return res
-
     def train(self, training_sents):
+        """ Trains the model by remembering the existing patterns of length n """
         for sent in training_sents:
             history = []
             for token in sent:
@@ -29,9 +24,22 @@ class HMMTagger(object):
                     del history[0]
 
     def next_tag(self, tagged_tokens, next_token):
+        """ Determins the POS tag for the next_token based on the given
+        tagged_tokens. Returns the tag """
         context = tuple(tagged_tokens + [next_token])
         print "context ", context
         if context in self.freqDist:
             return self.freqDist[context].max()
         else:
             return "UNK"
+
+    def tag(self, tokenized_sents):
+        """ Tags the given set of tokenized sentences. Returns a list of lists of tags. """
+        res = []
+        for sentence in tokenized_sents:
+            tagged_sentence = []
+            for token in sentence:
+                context = tagged_sentence[-self.n:]
+                tagged_sentence.append(self.next_tag(context, token))
+            res.append(tagged_sentence)
+        return res
