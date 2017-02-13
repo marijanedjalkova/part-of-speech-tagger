168a508e631f977643d7fb1ae6258b9233b003aa
Marija Nedjalkova marijanedjalkova@github.com <marijanedjalkova@github.com>
2017-02-13 19:12:39 +0000

    fixed frequencies and probabilities for tagged words and tags

--- HMMTagger.py
+++ HMMTagger.py
@@ -5,14 +5,23 @@ class HMMTagger(object):
 	def __init__(self, tagged_sents, n=0):
 		self.n = n
 		self.tagged_sents = tagged_sents
+		self.train()
 
-		self.freqDistTaggedWords = ConditionalFreqDist()
-		self.construct_freqDist()
-		self.probDist = ConditionalProbDist(self.freqDistTaggedWords, MLEProbDist)
-		self.freqDistTags= ConditionalFreqDist(bigrams(brown_tags))
+	def construct_frequencies(self):
+		self.construct_freqDistTaggedWords()
+		self.probDistTaggedWords = ConditionalProbDist(self.freqDistTaggedWords, MLEProbDist)
+		#extract tags from sentences
+		tags = []
+		for sent in self.tagged_sents:
+			tags.extend([t for (_,t) in sent])
+		# now tags is a list of tags
+		self.freqDistTags = ConditionalFreqDist(bigrams(tags))
+		# TODO change from bigrams to ngrams
+		self.probDistTags = ConditionalProbDist(self.freqDistTags, MLEProbDist)
 
 	def construct_ngram_freqDist(self):
 		""" Conditions are of form (tag, tag, tag, word) """
+		self.freqDistTaggedWords = ConditionalFreqDist()
 		for sent in self.tagged_sents:
 			history = []
 			for token in sent:
@@ -24,15 +33,12 @@ class HMMTagger(object):
 				if len(history) == (self.n+1):
 					del history[0]
 
-	def construct_freqDist(self):
-		self.freqDistTaggedWords = ConditionalFreqDist([j for i in self.tagges_sents for j in i])
+	def construct_freqDistTaggedWords(self):
+		self.freqDistTaggedWords = ConditionalFreqDist([j for i in self.tagged_sents for j in i])
 
 	def train(self):
-		#extract tags from sentences
-		tags = []
-		for sent in self.tagged_sents:
-			tags.extend([t for (_,t) in first])
-		# now tags is a list of lists of tags
+		self.construct_frequencies()
+		
 
 	def tag(self):
 		pass
@@ -41,6 +47,6 @@ if __name__ == '__main__':
 	from nltk.corpus import brown
 	from nltk import ConditionalProbDist, ConditionalFreqDist, MLEProbDist
 	sents = brown.tagged_sents()
-	#print sents[0]
+	print sents
 	hmt = HMMTagger(sents[:2])
 	#print hmt.probDist[("irregularities",)].prob("NNS")
