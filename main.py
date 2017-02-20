178f58f79251af6b7cf67b732f96fb0b52b92f62
Marija Nedjalkova marijanedjalkova@github.com <marijanedjalkova@github.com>
2017-02-20 23:40:43 +0000

    code refactoring on list comprehension

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ def main():
     sents = brown.tagged_sents()
     # 57340 sentences
     training_set = sents[:50000]
-    testing_set = sents[50000:50002]
-
+    testing_set = sents[:2]
     t = HMMTagger(training_set)
     test_words = [[w for (w,_) in sentence] for sentence in testing_set]
     test_tag_sents = [[tag for (_,tag) in sentence] for sentence in testing_set]
-    new_tags = t.tag(test_words) # are received as plain list
-    print compare(new_tags, test_tag_sents), "%"
+    new_tag_sents = t.tag_sents(test_words)
+    print compare(new_tag_sents, test_tag_sents), "%"
 
 def plain_to_sents(tags):
     res = []
@@ -29,18 +28,14 @@ def plain_to_sents(tags):
     return res
 
 def sents_to_plain(sents):
-    res = []
-    for s in sents:
-        res.append("<s>")
-        res.extend(s)
-        res.append("</s>")
-    return res
+    return [j for i in sents for j in i]
 
 
-def compare(detected_tags_lst, original_tags_sents):
+def compare(detected_tags_sents, original_tags_sents):
     res = 0
     total = 0
     original_tags_lst = sents_to_plain(original_tags_sents)
+    detected_tags_lst = sents_to_plain(detected_tags_sents)
     print "detected: ", detected_tags_lst
     print "original: ", original_tags_lst
     for detected, original in zip(detected_tags_lst, original_tags_lst):
