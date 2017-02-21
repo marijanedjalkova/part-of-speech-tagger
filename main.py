82d89eb68cfaa7d4f5e8cb2cdf3f8d5d877b6c9a
Marija Nedjalkova marijanedjalkova@github.com <marijanedjalkova@github.com>
2017-02-21 12:18:25 +0000

    code cleanup for main

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from nltk.corpus import brown
-from HMMTagger import HMMTagger
+from HMMTagger import *
 from NGramTagger import NGramTagger
 from nltk.tokenize import word_tokenize
 
 def main():
     sents = brown.tagged_sents()
     # 57340 sentences
-    training_set = sents[:50000]
+    training_set = sents[:5000]
     testing_set = sents[:2]
     t = HMMTagger(training_set)
-    test_words = [[w for (w,_) in sentence] for sentence in testing_set]
+    test_words = [[meow for (meow,_) in sentence] for sentence in testing_set]
     test_tag_sents = [[tag for (_,tag) in sentence] for sentence in testing_set]
     new_tag_sents = t.tag_sents(test_words)
     print compare(new_tag_sents, test_tag_sents), "%"
 
 def plain_to_sents(tags):
+    """ Parses a list of tags where sentences are separated by start and end tags into list of lists"""
     res = []
     small = []
     for t in tags:
-        if t=="<s>":
+        if t==START_TAG:
             continue
-        if t=="</s>":
+        if t==END_TAG:
             res.append(small)
             small = []
             continue
@@ -28,6 +29,7 @@ def plain_to_sents(tags):
     return res
 
 def sents_to_plain(sents):
+    """ Jois a list of lists into a plain list """
     return [j for i in sents for j in i]
 
 
