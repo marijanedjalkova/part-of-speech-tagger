a9eb285f5cdd7765cbaf3ad4f2b4eed6f6e1e309
Marija Nedjalkova marijanedjalkova@github.com <marijanedjalkova@github.com>
2017-02-25 17:23:53 +0000

    moved merging into initial stage of the process

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from nltk.corpus import brown
 from HMMTagger import *
-from NGramTagger import NGramTagger
-from nltk.tokenize import word_tokenize
 import sys, getopt
-import numbers
 import time
 
 smoothing = "LAP"
@@ -39,25 +36,65 @@ def process_args(argv, num_of_sents):
         else:
             continue
 
+def merge(sents, merging):
+    new_sents = []
+    for sent in sents:
+        new_sent = []
+        for (w,t) in sent:
+            new_tuple = (w,t[:2]) if t.lower().startswith(merging) else (w,t)
+            new_sent.append(new_tuple)
+        new_sents.append(new_sent)
+    return new_sents
+
+def process_data(sents, merging):
+    """Merge and split data into different sets.
+    Return training set, testing words and testing tags."""
+    merged_sents = merge(sents, merging)
+    training_set = merged_sents[ trainstart : trainstart + trainlength ]
+    testing_set = merged_sents[ trainstart + trainlength + 1 : trainstart + trainlength + 1 + testlength ]
+    test_words = [[meow for (meow,_) in sentence] for sentence in testing_set]
+    test_tag_sents = [[tag for (_,tag) in sentence] for sentence in testing_set]
+    return (training_set, test_words, test_tag_sents)
 
 def main(argv):
     sents = brown.tagged_sents()
     num_of_sents = 57340
-    # I know this is a magic number but Pythoon takes too long to count it every time.
-
+    # I know this is a magic number but len(sents) takes too long.
     process_args(argv, num_of_sents)
-
-    training_set = sents[trainstart:trainstart+trainlength]
-    testing_set = sents[trainstart+trainlength+1:trainstart+trainlength+1+testlength]
-    # the two previous lines take a lot of time
+    training_set, test_words, test_tag_sents = process_data(sents, ("be", "nn", "jj", "dt", "fw", "hv", "md", "np", "vb"))
     t = HMMTagger(training_set, smoothing=smoothing)
 
+    new_tag_sents = t.tag_sents(test_words)
+    #print new_tag_sents
+    print "all merging: ", compare(new_tag_sents, test_tag_sents), "%"
+    print "Accuracy for nouns: ", measure_accuracy_for_class(new_tag_sents, test_tag_sents, "NN"), "%"
+    print "Accuracy for adjectives: ", measure_accuracy_for_class(new_tag_sents, test_tag_sents, "JJ"), "%"
+
+    merged_sents = sents
+    training_set = merged_sents[ trainstart : trainstart + trainlength ]
+    testing_set = merged_sents[ trainstart + trainlength + 1 : trainstart + trainlength + 1 + testlength ]
     test_words = [[meow for (meow,_) in sentence] for sentence in testing_set]
     test_tag_sents = [[tag for (_,tag) in sentence] for sentence in testing_set]
-
+    t = HMMTagger(training_set, smoothing=smoothing)
     new_tag_sents = t.tag_sents(test_words)
-    print compare(new_tag_sents, test_tag_sents), "%"
-    print measure_accuracy_for_class(new_tag_sents, test_tag_sents, "NN"), "%"
+    #print new_tag_sents
+    print "No merging: ", compare(new_tag_sents, test_tag_sents), "%"
+    print "Accuracy for nouns: ", measure_accuracy_for_class(new_tag_sents, test_tag_sents, "NN"), "%"
+    print "Accuracy for adjectives: ", measure_accuracy_for_class(new_tag_sents, test_tag_sents, "JJ"), "%"
+    return
+    t = HMMTagger(training_set, smoothing=smoothing)
+    new_tag_sents = t.tag_sents(test_words)
+    #print new_tag_sents
+    print "Adjective merging: ", compare(new_tag_sents, test_tag_sents), "%"
+    print "Accuracy for nouns: ", measure_accuracy_for_class(new_tag_sents, test_tag_sents, "NN"), "%"
+    print "Accuracy for adjectives: ", measure_accuracy_for_class(new_tag_sents, test_tag_sents, "JJ"), "%"
+
+    t = HMMTagger(training_set, smoothing=smoothing)
+    new_tag_sents = t.tag_sents(test_words)
+    #print new_tag_sents
+    print "Noun and adjective merging: ", compare(new_tag_sents, test_tag_sents), "%"
+    print "Accuracy for nouns: ", measure_accuracy_for_class(new_tag_sents, test_tag_sents, "NN"), "%"
+    print "Accuracy for adjectives: ", measure_accuracy_for_class(new_tag_sents, test_tag_sents, "JJ"), "%"
     # TODO with merging, accuracy of the merged class grows but the accuracy of the text overall falls?
     # if merge too much, lose context
 
